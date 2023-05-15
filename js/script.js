9f437a54504dfa6ddb25651622cac2f018275af0
Mazen Mohamed <132310334+Mazen-WebDev@users.noreply.github.com>
2023-05-16 01:51:16 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 let burgerIcon = document.querySelector(".burger-icon");
 let burgerIconSpan = document.querySelectorAll(".burger-icon span");
 let navBar = document.querySelector("nav");
+let uListLi = document.querySelectorAll("ul li a");
 
 burgerIcon.addEventListener("click", () => {
     navBar.classList.toggle("open");
     burgerIconSpan.forEach((span) => {
         span.classList.toggle("close");
+    });
+});
+
+uListLi.forEach((a) => {
+    a.addEventListener("click", () => {
+        navBar.classList.remove("open");
+        burgerIconSpan.forEach((span) => {
+            span.classList.remove("close");
+        });
     });
 });
